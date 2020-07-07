ed097acf76391b27be811cd2c4a840888bdbee86
Orizz <dimaorizz@gmail.com>
2020-07-07 20:24:06 +0300

    api fetch added

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,24 @@
-import React from 'react'
+import React from 'react';
+import darksky from '../api/darksky';
+import mapbox from '../api/mapbox';
 
 class App extends React.Component {
+
+    state = {
+        place: '',
+        temperature: null,
+    }
+
+    async componentDidMount() {
+        const mapBoxData = await mapbox(this.state.place);
+        const location = {
+            lat: mapBoxData.features[0].center[1],
+            long: mapBoxData.features[0].center[0],
+        }
+        const forecast = await darksky(location);
+        console.log(forecast.currently.temperature)
+    }
+
     render() {
         return (
             <div>
