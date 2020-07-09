5845eaa41207f94d145d03c8c348fa14c9372668
Orizz <dimaorizz@gmail.com>
2020-07-09 19:12:02 +0300

    Updates

---
+++ precache-manifest.baecfa8aca4f6f46d90f38e5cf9881b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42a34cfc25a67043108904883d52e8c2",
+    "url": "/react-weather-app/index.html"
+  },
+  {
+    "revision": "891b3734bb14aee5f7cc",
+    "url": "/react-weather-app/static/js/2.f46ab68f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-weather-app/static/js/2.f46ab68f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd5166ac5edb362f955d",
+    "url": "/react-weather-app/static/js/main.e23e8678.chunk.js"
+  },
+  {
+    "revision": "a165d426a6e734b12112",
+    "url": "/react-weather-app/static/js/runtime-main.ec1803de.js"
+  }
+]);
