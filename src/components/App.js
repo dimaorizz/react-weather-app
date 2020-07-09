ca6428876ce3feeb75f1b1765fc430147988862f
Orizz <dimaorizz@gmail.com>
2020-07-09 16:25:35 +0300

    infinite render fix

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,22 @@ import React from 'react';
 import darksky from '../api/darksky';
 import mapbox from '../api/mapbox';
 import TempInfo from './TempInfo/TempInfo';
+import PlaceInput from './PlaceInput/PlaceInput'
 
 class App extends React.Component {
 
     state = {
-        place: 'Запорожье',
+        place: '',
         temperature: null,
     }
 
-    async componentDidMount() {
+    onFormSubmit = (place) => {
+        this.setState({ place: place });
+    };
+
+    async componentDidUpdate() {
         const mapBoxData = await mapbox(this.state.place);
+        console.log(mapBoxData);
         const location = {
             lat: mapBoxData.features[0].center[1],
             long: mapBoxData.features[0].center[0],
@@ -20,9 +26,17 @@ class App extends React.Component {
         this.setState({temperature: forecast.currently.temperature})
     }
 
+    shouldComponentUpdate(nextProps, nextState) {
+        if(JSON.stringify(this.state) === JSON.stringify(nextState)) {
+            return false;
+        }
+        return true;
+    }
+
     render() {
         return (
             <div>
+                <PlaceInput onFormSubmit={this.onFormSubmit} />
                 <TempInfo place={this.state.place} temperature={this.state.temperature} />
             </div>
         )
---
+++ src/components/PlaceInput/PlaceInput.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+class PlaceInput extends React.Component {
+    state = {
+        place: ''
+    }
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.props.onFormSubmit(this.state.place);
+    }
+
+    render() {
+        return(
+            <div className='placeInput'>
+                <form onSubmit={this.onSubmit}>
+                    <input type='text' onChange={(e) => this.setState({ place: e.target.value })} value={this.state.place} />
+                </form>
+            </div>
+        )
+    }
+
+};
+
+export default PlaceInput;
